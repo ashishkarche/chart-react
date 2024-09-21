9aadb661f0e0f9aa0119461e0dd2dfa0ce289c9d
ashishkarche <ashishkarche9@gmail.com>
2024-09-21 13:53:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [tradeData, setTradeData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Fetch data with basic auth
+    axios.get('/get-trade-general/20240921/chart', {
+      auth: {
+        username: 'tradesafeapi',
+        password: 'Ashish0921'
+      }
+    })
+    
+      .then((response) => {
+        setTradeData(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError('Error fetching data');
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading data...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Trade Data</h1>
+      <table border="1" cellPadding="10">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Score</th>
+            <th>Last Price</th>
+            <th>Green Candles</th>
+            <th>Red Candles</th>
+            <th>Stable Candles</th>
+            <th>Green Candles Time</th>
+            <th>Red Candles Time</th>
+            <th>Computed Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(tradeData.Name).map((key) => (
+            <tr key={key}>
+              <td>{tradeData.Name[key]}</td>
+              <td>{tradeData.Score[key]}</td>
+              <td>{tradeData.LastPrice[key]}</td>
+              <td>{tradeData["Green Candels"][key]}</td>
+              <td>{tradeData["Red Candels"][key]}</td>
+              <td>{tradeData["Stable Candels"][key]}</td>
+              <td>{tradeData["Green Candles Time"][key]}</td>
+              <td>{tradeData["Red Candles Time"][key]}</td>
+              <td>{tradeData.computed_time[key]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
